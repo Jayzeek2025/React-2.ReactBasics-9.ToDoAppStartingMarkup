e67e28232bf641ae0724d2a15ce673452ce452eb
Juan Carlos Kerchaten <jcfiles07@gmail.com>
2026-01-13 22:31:00 +0900

    fixed checkboxes

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,18 +1,24 @@
-//Task
-function Task({title,completed}){
-   return (
-      <li className="todo-item">
-         <div className="view">
-            <input className ="checkbox" 
-            type="checkbox"
-            checked={completed}
-            readOnly/>
-            <label>{title}</label>
-            <button className="icon icon-edit"></button>
-            <button className="icon icon-destroy"></button>
-         </div>
-      </li>
-   );
+function Task({ title, completed }) {
+  return (
+    <li className={completed ? "completed" : ""}>
+      <div className="view">
+        <input
+          className="toggle"
+          type="checkbox"
+          checked={completed}
+          readOnly
+        />
+
+        <label>
+          <span className="description">{title}</span>
+          <span className="created">created just now</span>
+        </label>
+
+        <button className="icon icon-edit"></button>
+        <button className="icon icon-destroy"></button>
+      </div>
+    </li>
+  );
 }
 
 export default Task;
